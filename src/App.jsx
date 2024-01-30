37e2d16cdf1e0fd8e045b81f4496d44601d395e9
fquea15 <qjcoders@gmail.com>
2024-01-30 18:56:50 -0500

    Added functionality to switch between dark and light themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,56 @@
-import Clients from './components/Clients'
-import Footer from './components/Footer'
-import Free from './components/Free'
-import Home from './components/Home'
-import Like from './components/Like'
-import Navbar from './components/Navbar'
-import Release from './components/Release'
-import ScrollToTop from './components/ScrollToTop'
-import Signup from './components/Signup'
-import SuperRare from './components/SuperRare'
-
+import { useEffect, useState } from "react";
+import ScrollReveal from "scrollreveal";
+import Clients from "./components/Clients";
+import Footer from "./components/Footer";
+import Free from "./components/Free";
+import Home from "./components/Home";
+import Like from "./components/Like";
+import Navbar from "./components/Navbar";
+import Release from "./components/Release";
+import ScrollToTop from "./components/ScrollToTop";
+import Signup from "./components/Signup";
+import SuperRare from "./components/SuperRare";
 
 // Styles
-import "./scss/index.scss"
+import "./scss/index.scss";
 
 export default function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const changeTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
+
+  useEffect(() => {
+    const registerAnimations = () => {
+      const sr = ScrollReveal({
+        origin: "bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false,
+      });
+
+      sr.reveal(
+        `nav, .home, .free, .clients, .super-rare, .releases, .like, .signup, .footer`,
+        {
+          interval: 500,
+        }
+      );
+    };
+    registerAnimations();
+  }, []);
+
+  window.setTimeout(() => {
+    const home = document.getElementsByClassName("home");
+    home[0].style.transform = "none";
+    const nav = document.getElementsByTagName("nav");
+    nav[0].style.transform = "none";
+  }, 1500);
+
   return (
-    <div className="app-container">
+    <div className="app-container" data-theme={theme}>
       <ScrollToTop />
-      <Navbar />
+      <Navbar changeTheme={changeTheme} currentTheme={theme}/>
       <Home />
       <Free />
       <Clients />
@@ -27,5 +60,5 @@ export default function App() {
       <Signup />
       <Footer />
     </div>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
-import { ImSun } from "react-icons/bs";
+import { ImSun } from "react-icons/im";
 import { BsFillMoonFill } from "react-icons/bs";
 import logo from "../assets/logo.png";
 import { useState } from "react";
 
-export default function Navbar() {
+export default function Navbar({ changeTheme, currentTheme }) {
   const [navState, setNavState] = useState(false);
 
   return (
@@ -16,7 +16,13 @@ export default function Navbar() {
         </div>
         <div className="toggle-container">
           <div className="toggle"></div>
-          <div className="mode"></div>
+          <div className="mode">
+            {currentTheme === "dark" ? (
+              <ImSun className="light" />
+            ) : (
+              <BsFillMoonFill className="dark" />
+            )}
+          </div>
         </div>
       </div>
 
@@ -33,6 +39,13 @@ export default function Navbar() {
           </li>
           <li>
             <a href="#">Sing Up</a>
+          </li>
+          <li onClick={changeTheme}>
+            {currentTheme === "dark" ? (
+              <ImSun className="light" />
+            ) : (
+              <BsFillMoonFill className="dark" />
+            )}
           </li>
         </ul>
       </div>
